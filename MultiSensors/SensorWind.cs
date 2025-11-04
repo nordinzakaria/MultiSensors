6d1df88f3325b6a957c77f8008ef304fed07da2b
nordinzakaria <nordinzakaria@gmail.com>
2025-11-04 21:28:26 +0800

    updated

---
+++ MultiSensors/SensorWind.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiSensors
+{
+    internal class SensorWind: SensorBase
+    {
+        List<Vector2D> windata;
+        public override void Read() 
+        { 
+                    
+        }
+
+        public override float CalculateAverage() { return 0; }
+
+        public override void GetHigher() { }
+
+    }
+}
