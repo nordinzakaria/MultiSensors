38c746d223db11b39ac3bdb87e4ab381ba2f572d
nordinzakaria <nordinzakaria@gmail.com>
2025-11-04 20:55:40 +0800

    Initial commit

---
+++ MultiSensors/SensorBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiSensors
+{
+    internal abstract class SensorBase
+    {
+        public string Id { get; set; }
+        public string SerialNumber { get; set; }
+
+        public SensorBase() 
+        {
+            Console.WriteLine("Blank base class constructor");
+        }
+
+        public void DisplayHardwareInfo()
+        {
+            Console.WriteLine($"{Id} {SerialNumber}");
+        }
+
+    }
+}
