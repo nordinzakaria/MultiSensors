1a0a661217cd2f10b6bb632460d7e87695e63e1b
nordinzakaria <nordinzakaria@gmail.com>
2025-11-04 21:32:50 +0800

    Update Program.cs

--- MultiSensors/Program.cs
+++ MultiSensors/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MultiSensors;
+using System.Runtime.ExceptionServices;
+
 Console.WriteLine("Hello, World!");
+
+// test out the classes!
+
+List<SensorBase> sensors = new List<SensorBase>();
+
+SensorTemperature sensorTemperature = new SensorTemperature();
+SensorTemperature sensorTemperature2 = new SensorTemperature();
+SensorWind wind = new SensorWind();
+SensorWind wind2 = new SensorWind();
+
+sensors.Add(sensorTemperature);
+sensors.Add(sensorTemperature2);
+sensors.Add(wind);
+sensors.Add(wind2);
+
+foreach (SensorBase sensor in sensors)
+{
+    sensor.Read();
+}
